2c370390ccf2298210db649be31c9a16d81b8e3f
lye110502 <lye110502@gmail.com>
2026-07-10 13:20:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz2TbbRs49nIkZET3Nc_PDX-8Ylx38pr9NZYcbuzXWZH-Wo1f3bLcGr1EfsfI4WEqRh/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyH0dvRz2VW9TlbVRpmdArL59Ptq4pQIJgsh96vkWA08QdFjyqptBVgU9THzjZ-TYTz/exec";
 const DEFAULT_PASSWORD = "0418";
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyCS5jVD_7h1hVUl7vTALZdqPFR4azXkghw",
